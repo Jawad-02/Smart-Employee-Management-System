8b035b16ba48de5335e283ff22c66203aa90ff18
Jawad <jouad-2002@hotmail.com>
2026-05-24 03:20:39 +0300

    fix forceScheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\AbstractPaginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         AbstractPaginator::defaultView('pagination::bootstrap-5');
         AbstractPaginator::defaultSimpleView('pagination::simple-bootstrap-5');
     }
